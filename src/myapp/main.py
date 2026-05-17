b12f9ed5c6396061b7754d938ccaafd60e30ed88
Nicholas Chaves <tile27@proton.me>
2026-05-17 13:13:44 -0400

    feat: added opening greeting

---
+++ src/myapp/main.py
@@ -0,0 +1,7 @@
+from myapp.ui.greeting import primary_greeting
+
+def main():
+    primary_greeting()
+
+if __name__ == "__main__":
+    main()
---
+++ src/myapp/ui/greeting.py
@@ -0,0 +1,2 @@
+def primary_greeting():
+    print("Hello! Welcome to Litmanager!\nPlease choose from an option below:")
