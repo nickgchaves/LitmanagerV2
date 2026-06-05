d24212764c304c5434cc3af1e9feb88577d63cfa
Nicholas Chaves <tile27@proton.me>
2026-06-05 16:59:39 -0400

    feat: added basic runtime functionality

--- src/myapp/main.py
+++ src/myapp/main.py
@@ -2,7 +2,26 @@ from myapp.ui.user_messages import  user_options
 
 def main():
     print("Hello, welcome to Litmanager!")
-    user_options()
+
+    run_program = True
+    while run_program:
+        user_options()
+        user_choice = input("Your choice: ")
+        match user_choice:
+            case "1":
+                print("Test 1")
+            case "2":
+                print("Test 2")
+            case "3":
+                print("Test 3")
+            case "9":
+                print("Test 9")
+            case "0":
+                run_program = False
+                print("Hope to see you soon!")
+            case _:
+                print("Error: Invalid entry\n")
+
 
 if __name__ == "__main__":
     main()
