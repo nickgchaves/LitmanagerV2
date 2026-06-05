c84c37139c65a8728988d6b7daf71d232f85b892
Nicholas Chaves <tile27@proton.me>
2026-06-05 16:38:44 -0400

    feat: added validation logic to Book class

--- src/myapp/models/book.py
+++ src/myapp/models/book.py
@@ -3,4 +3,22 @@ class Book:
         self.title = title
         self.author = author
 
-    
+    @property
+    def author(self) -> str:
+        return self._author
+
+    @author.setter
+    def author(self, author_string: str) -> None:
+        if not author_string:
+            raise ValueError("Author cannot be empty")
+        self._author = author_string
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @title.setter
+    def title(self, title_string: str) -> None:
+        if not title_string:
+            raise ValueError("Title cannot be empty")
+        self._title = title_string
