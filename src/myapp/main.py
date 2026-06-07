cc62af7220d07075c9fd7715158843c05f7ea4b8
Nicholas Chaves <tile27@proton.me>
2026-06-07 10:06:32 -0400

    feat: added error handling to book instantiation

--- src/myapp/main.py
+++ src/myapp/main.py
@@ -3,21 +3,26 @@ from myapp.utils.book_utils import instantiate_book
 
 def main():
     print("Hello, welcome to Litmanager!")
-
+    library = []
     run_program = True
     while run_program:
         user_options()
         user_choice = input("Your choice: ")
         match user_choice:
-            case "1":
-                instantiate_book()
-            case "2":
+            case "1": # add book
+                try:
+                    book = instantiate_book()
+                    library.append(book)
+                    print("Congrats on the new book!")
+                except ValueError as error:
+                    print(f"Error: {error}")
+            case "2": # update book
                 print("Test 2")
-            case "3":
+            case "3": # delete book
                 print("Test 3")
-            case "9":
+            case "9": # about Litmanager
                 print("Test 9")
-            case "0":
+            case "0": # quit program
                 run_program = False
                 print("Hope to see you soon!")
             case _:
